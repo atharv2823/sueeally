813c5a91fbd86d0eac541fdbea939f63f74a836e
athav2823 <atharvaneware@gmail.com>
2025-03-08 21:59:29 +0530

    whatsapp

---
+++ app/_components/whatsapp.jsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { FaWhatsapp } from "react-icons/fa";
+
+const WhatsAppButton = () => {
+  const phoneNumber = "9130895209"; // Replace with your WhatsApp number
+
+  return (
+    <a
+      href={`https://wa.me/${phoneNumber}`}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="fixed bottom-32 right-5 bg-green-500 text-white p-3 rounded-full shadow-lg hover:bg-green-600 transition flex items-center justify-center w-14 h-14"
+    >
+      <FaWhatsapp size={30} />
+    </a>
+  );
+};
+
+export default WhatsAppButton;
--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import Navbar from "./_components/navbar";
 import Footer from "./_components/footer";
 import Top from "./_components/top"
+import WhatsAppButton from "./_components/whatsapp";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,6 +29,7 @@ export default function RootLayout({ children }) {
          <Navbar/>
         {children}
         <Footer/>
+        <WhatsAppButton/>
         <Top/>
       </body>
     </html>
