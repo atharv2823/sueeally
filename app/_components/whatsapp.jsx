9ee1114a81e5d6f44ddb19176675b001c07076ed
athav2823 <atharvaneware@gmail.com>
2025-03-29 14:02:28 +0530

    whtsapp

--- app/_components/whatsapp.jsx
+++ app/_components/whatsapp.jsx
@@ -1,4 +1,3 @@
-
 "use client";
 
 import { FaWhatsapp } from "react-icons/fa";
@@ -12,11 +11,11 @@ const WhatsAppButton = () => {
         href={`https://wa.me/${phoneNumber}`}
         target="_blank"
         rel="noopener noreferrer"
-        className="fixed bottom-32 right-5 bg-green-500 text-white p-3 rounded-full shadow-lg hover:bg-green-600 transition flex items-center justify-center w-14 h-14"
+        className="fixed bottom-32 right-5 bg-green-500 text-white p-3 rounded-full shadow-lg hover:bg-green-600 transition flex items-center justify-center w-14 h-14 z-50"
       >
         <FaWhatsapp size={30} />
       </a>
-      <p className="fixed bottom-24 right-5 bg-green-500 text-white px-3 py-1 rounded-lg shadow-md text-sm font-semibold">
+      <p className="fixed bottom-24 right-5 bg-green-500 text-white px-3 py-1 rounded-lg shadow-md text-sm font-semibold z-50 animate-bounce">
         Chat for Instant QUOTE
       </p>
     </>
